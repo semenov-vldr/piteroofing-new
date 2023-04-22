1121945d4dad6674e533bc947d462a19c55bceba
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-04-22 08:17:58 +0000

    deploy: 6f9e2c3cb4c1f2b029d9382d1a959949226c3ba7

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+const header = document.querySelector('header.header');
+
+const burger = header.querySelector('.header__burger');
+
+burger.addEventListener('click', () => {
+  header.classList.add('js-menu-open')
+})
+
+const headerArrow = header.querySelector('.header__nav-arrow');
+
+headerArrow.addEventListener('click', () => {
+  header.classList.remove('js-menu-open')
+})
