cb3d6d620e2ae95e1985cabbb47b082f75ce2d0d
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-05-09 16:10:32 +0000

    deploy: 4a867e3a96f60eac7a26840b18cab3c89317a17a

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,84 @@
 
+{
+
+  let mySwiper;
+
+  const swiperList = document.querySelectorAll('.gallery__slider');
+
+  if (swiperList) createSwiper(swiperList);
+
+
+
+  function createSwiper (swiperList) {
+    swiperList.forEach(swiper => {
+
+      mySwiper = new Swiper(swiper, {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.slider-nav__next',
+          prevEl: '.slider-nav__prev',
+        },
+
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        //   draggable: true,
+        // },
+
+        uniqueNavElements: true,
+
+        //slidesPerView: 1.5,
+
+        // Бесконечная прокрутка
+        loop: true,
+
+        // Откл функционала, если слайдов меньше, чем нужно
+        watchOverflow: true,
+
+        centeredSlides: true,
+
+        // Отступ между слайдами
+        //spaceBetween: 24,
+
+        // Стартовый слайд
+        initialSlide: 0,
+
+        // Брейк поинты (адаптив)
+        // Ширина экрана
+        breakpoints: {
+          320: {
+            slidesPerView: 1.2,
+            spaceBetween: 16,
+          },
+
+          768: {
+            slidesPerView: 1.5,
+            spaceBetween: 150,  // 24
+          },
+        }
+
+      });
+
+
+      const images = swiper.querySelectorAll('.swiper-slide img');
+      images.forEach(image => {
+        image.setAttribute('data-src', image.src);
+        image.setAttribute('data-fancybox', "gallery");
+      });
+
+
+
+    })
+
+
+
+  };
+
+
+}
+
 const header = document.querySelector('header.header');
 
 const burger = header.querySelector('.header__burger');
@@ -23,8 +103,20 @@ function scrollHeader (header) {
 
   if (previousPosition > currentPosition || window.scrollY < 100) {
     header.classList.remove('js-scroll');
+    header.classList.add('js-header-bg');
   } else {
     header.classList.add('js-scroll');
+    header.classList.remove('js-header-bg');
   }
+
+  if (window.scrollY < 100) {
+    header.classList.remove('js-header-bg');
+  }
+
   previousPosition = currentPosition;
+
+
+
+
+
 }
