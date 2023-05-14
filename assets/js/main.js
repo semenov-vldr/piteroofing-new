82af969c4be557cfe1e27a61f091b6d4002c7f43
semenov-vldr <semenov-vldr@users.noreply.github.com>
2023-05-14 09:13:02 +0000

    deploy: 946097cf892be81fc738eb0952d8afccc2071801

--- assets/js/main.js
+++ assets/js/main.js
@@ -97,6 +97,97 @@ headerArrow.addEventListener('click', () => {
 // scroll
 window.addEventListener('scroll', () => scrollHeader (header) );
 
+{
+
+  let mySwiper;
+
+  const swiperList = document.querySelectorAll('.reviews__slider');
+
+  if (swiperList) createSwiper(swiperList);
+
+
+
+  function createSwiper (swiperList) {
+    swiperList.forEach(swiper => {
+
+      mySwiper = new Swiper(swiper, {
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.slider-nav__next',
+          prevEl: '.slider-nav__prev',
+        },
+
+        // scrollbar: {
+        //   el: '.swiper-scrollbar',
+        //   draggable: true,
+        // },
+
+        uniqueNavElements: true,
+
+        //slidesPerView: 1.5,
+
+        // Бесконечная прокрутка
+        loop: true,
+
+
+        // Откл функционала, если слайдов меньше, чем нужно
+        watchOverflow: true,
+
+        centeredSlides: true,
+
+        // Отступ между слайдами
+        //spaceBetween: 24,
+
+        // Стартовый слайд
+        initialSlide: 0,
+
+        // Брейк поинты (адаптив)
+        // Ширина экрана
+        breakpoints: {
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 16,
+          },
+
+          480: {
+            slidesPerView: 1.5,
+          },
+
+
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 32,
+          },
+
+          1100: {
+            slidesPerView: 2.5,
+            spaceBetween: 40,  // 24
+          },
+        }
+
+      });
+
+
+      const images = swiper.querySelectorAll('.swiper-slide img');
+      images.forEach(image => {
+        image.setAttribute('data-src', image.src);
+        image.setAttribute('data-fancybox', "gallery");
+      });
+
+
+
+    })
+
+
+
+  };
+
+
+}
+
 let previousPosition = document.documentElement.scrollTop;
 function scrollHeader (header) {
   let currentPosition = document.documentElement.scrollTop;
